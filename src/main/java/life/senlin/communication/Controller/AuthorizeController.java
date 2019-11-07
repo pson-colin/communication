b1a3eddbe93c7c91749ae9a8def7136b39332d3c
colin <lin2576607565@163.com>
2019-11-07 14:20:56 +0800

    update config from code to properties

--- src/main/java/life/senlin/communication/Controller/AuthorizeController.java
+++ src/main/java/life/senlin/communication/Controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.senlin.communication.Provider.GithubProvider;
 import life.senlin.communication.dto.AccessTokenDTO;
 import life.senlin.communication.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,21 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
     @GetMapping("/callback")
     public String callback(@RequestParam(value = "code") String code,
                            @RequestParam(value = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("597f22d0a6adce2ca49b");
-        accessTokenDTO.setClient_secret("8f46893d7dd4fd1f7f416661d9b04813e1e3b553");
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
