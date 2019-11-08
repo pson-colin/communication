806ef0a2c5091350ac20c508d381e07d1415bc4b
colin <lin2576607565@163.com>
2019-11-08 12:19:49 +0800

    set check cookies

--- src/main/java/life/senlin/communication/mapper/UserMapper.java
+++ src/main/java/life/senlin/communication/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.senlin.communication.mapper;
 import life.senlin.communication.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @Author: colin
@@ -12,4 +14,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modify) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModify});")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
