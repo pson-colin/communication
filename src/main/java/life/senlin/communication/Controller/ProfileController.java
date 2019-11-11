4040c138b9955b3f5320214a449f261434a768dd
colin <lin2576607565@163.com>
2019-11-11 19:38:21 +0800

    create user personal question page and repair pagination function

--- src/main/java/life/senlin/communication/Controller/ProfileController.java
+++ src/main/java/life/senlin/communication/Controller/ProfileController.java
@@ -1,8 +1,66 @@
 package life.senlin.communication.Controller;
 
+import life.senlin.communication.dto.PaginationDTO;
+import life.senlin.communication.mapper.QuestionMapper;
+import life.senlin.communication.mapper.UserMapper;
+import life.senlin.communication.model.User;
+import life.senlin.communication.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Author: colin
  * @Date: 17:39 2019/11/11
  */
+@Controller
 public class ProfileController {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable("action") String action,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "5") Integer size,
+                          Model model,
+                          HttpServletRequest request) {
+        //校验登录信息
+        Cookie[] cookies = request.getCookies();
+        User user = null;
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        if (user == null) {
+            return "redirect:/";
+        }
+        //控制页面的文本显示
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+        //查找用户的问题列表
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);
+        return "profile";
+    }
 }
--- src/main/java/life/senlin/communication/dto/PaginationDTO.java
+++ src/main/java/life/senlin/communication/dto/PaginationDTO.java
@@ -21,20 +21,8 @@ public class PaginationDTO {
     private Integer totalPage;
     private List<Integer> pages = new ArrayList<Integer>();
 
-    public void setPagination(Integer totalCount, Integer page, Integer size) {
-
-        //总页数
-        if (totalCount % size == 0) {
-            totalPage = totalCount / size;
-        } else {
-            totalPage = totalCount / size + 1;
-        }
-        if(page < 1){
-            page = 1;
-        }
-        if(page > totalPage){
-            page = totalPage;
-        }
+    public void setPagination(Integer totalPage, Integer page) {
+        this.totalPage = totalPage;
         this.currentPage = page;
         //初始化pages数组
         pages.add(page);
